58f25b4bfaf2137fbe03457a48ec775d0364dee7
Brian Lawhon <brianlawhon@live.com>
2022-03-26 15:16:41 -0500

    Add a method that will perform a single round git status

--- javascript.js
+++ javascript.js
@@ -7,16 +7,46 @@ function computerPlay(){
     let value = Math.floor(Math.random()*3 + 1);
     
     if(value === 1){
-        return "Rock";
+        return "rock";
     }
     else if(value == 2){
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissors";
+        return "scissors";
     }
     
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+    let playerChoice = playerSelection.toLowerCase();
+
+    if(playerChoice === "rock" && computerSelection === "rock"){
+        return "Tie!";
+    }
+    else if(playerChoice === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper beats Rock";
+    }
+    else if(playerChoice === "rock" && computerSelection === "scissors"){
+        return "You Win! Rock beats Scissors";
+    }
+    else if(playerChoice === "paper" && computerSelection === "rock"){
+        return "You Win! Paper beats Rock";
+    }
+    else if(playerChoice === "paper" && computerSelection === "paper"){
+        return "Tie!";
+    }
+    else if(playerChoice === "paper" && computerSelection === "scissors"){
+        return "You Lose! Scissors beats Paper";
+    }
+    else if(playerChoice === "scissors" && computerSelection === "rock"){
+        return "You Lose! Rock beats Scissors";
+    }
+    else if(playerChoice === "scissors" && computerSelection === "paper"){
+        return "You Win! Scissors beats Paper";
+    }
+    else{
+        return "Tie!";
+    }
+}
 
