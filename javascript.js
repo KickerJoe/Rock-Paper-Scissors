2d0fb03265cb5f47241bd00753c35f63ad512e20
Brian Lawhon <brianlawhon@live.com>
2022-03-26 14:58:29 -0500

    Add a computer function that will choose rock,paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 //Rock Paper Scissors Program from Odin Project
+
+//Function that controls the computer
+function computerPlay(){
+    
+    //value will hold the number for the computers choice.
+    let value = Math.floor(Math.random()*3 + 1);
+    
+    if(value === 1){
+        return "Rock";
+    }
+    else if(value == 2){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+    
+}
+
+console.log(computerPlay());
+
